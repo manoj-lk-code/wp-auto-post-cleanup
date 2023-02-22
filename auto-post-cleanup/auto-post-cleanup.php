b0e1545de53f04d992b1a9bc2d5e05ad4e51746a
Manoj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2023-02-23 04:32:26 +0530

    Update auto-post-cleanup.php

--- auto-post-cleanup/auto-post-cleanup.php
+++ auto-post-cleanup/auto-post-cleanup.php
@@ -1,9 +1,8 @@
-<?php
 /*
 Plugin Name: Auto Post Cleanup
 Plugin URI: https://wpzonify.com/
 Description: Deletes all posts and their associated images that are older than 90 days.
-Version: 1.0
+Version: 1.1
 Author: Manoj lk
 Author URI: https://manojlk.work/
 */
@@ -13,19 +12,27 @@ add_action( 'init', 'auto_post_cleanup' );
 function auto_post_cleanup() {
     $args = array(
         'post_type'      => 'post',
-        'posts_per_page' => -1,
+        'posts_per_page' => 50, // limit to 50 posts per batch
         'post_status'    => 'publish',
         'date_query'     => array(
             array(
                 'column' => 'post_date',
-                'before' => '90 days ago',
+                'before' => '90 days ago', // delete older post then set days here
             ),
         ),
     );
 
+    $offset = 0;
     $posts = get_posts( $args );
 
-    foreach ( $posts as $post ) {
-        wp_delete_post( $post->ID, true );
+    while ( $posts ) {
+        foreach ( $posts as $post ) {
+            wp_delete_post( $post->ID, true );
+        }
+
+        $offset += 50; // increment the offset
+        $args['offset'] = $offset; // set the new offset in the query
+
+        $posts = get_posts( $args ); // fetch the next batch of posts
     }
 }
