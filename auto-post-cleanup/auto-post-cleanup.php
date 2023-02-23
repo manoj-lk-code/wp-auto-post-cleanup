7c2622c118787dc66af73ffa76c8a468e2ff8afb
Manoj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2023-02-23 05:40:04 +0530

    Updated the code that runs based on WPCron.

--- auto-post-cleanup/auto-post-cleanup.php
+++ auto-post-cleanup/auto-post-cleanup.php
@@ -1,38 +1,60 @@
-/*
-Plugin Name: Auto Post Cleanup
-Plugin URI: https://wpzonify.com/
-Description: Deletes all posts and their associated images that are older than 90 days.
-Version: 1.1
-Author: Manoj lk
-Author URI: https://manojlk.work/
-*/
+<?php
+/**
+ * Plugin Name: Auto Post Cleanup
+ * Plugin URI: https://wpzonify.com/
+ * Description: Deletes all posts and their associated images that are older than 90 days.
+ * Version: 1.1
+ * Author: Manoj lk
+ * Author URI: https://manojlk.work/
+ */
 
-add_action( 'init', 'auto_post_cleanup' );
+// Register the cron event
+add_action( 'wp', 'schedule_auto_post_cleanup' );
 
-function auto_post_cleanup() {
+/**
+ * Schedule the auto post cleanup to run immediately
+ */
+
+add_action( 'wp', 'schedule_auto_post_cleanup' );
+
+function schedule_auto_post_cleanup() {
+    if ( ! wp_next_scheduled( 'auto_post_cleanup' ) ) {
+        $now = time();
+        wp_schedule_single_event( $now - 1, 'auto_post_cleanup' );
+    }
+}
+
+
+// Register the cleanup function with the cron event
+add_action( 'auto_post_cleanup', 'cleanup_old_posts' );
+
+/**
+ * Deletes up to 50 old posts and their associated meta data and images
+ */
+function cleanup_old_posts() {
+    // Only run the cleanup function if the current request was triggered by the WordPress cron
+    if ( ! defined( 'DOING_CRON' ) || ! DOING_CRON ) {
+        return;
+    }
+
+    // Set up the query parameters to find old posts
     $args = array(
         'post_type'      => 'post',
-        'posts_per_page' => 50, // limit to 50 posts per batch
+        'posts_per_page' => 5, // limit to 5 posts per batch. this is using only 5 to avoid high load on server. you can adjust it to whatever you want. I recommend 50-100 value.
         'post_status'    => 'publish',
         'date_query'     => array(
             array(
                 'column' => 'post_date',
-                'before' => '90 days ago', // delete older post then set days here
+                'before' => '90 days ago', // delete older posts than 90 days
             ),
         ),
     );
 
-    $offset = 0;
+    // Get the posts that match the query
     $posts = get_posts( $args );
 
-    while ( $posts ) {
-        foreach ( $posts as $post ) {
-            wp_delete_post( $post->ID, true );
-        }
-
-        $offset += 50; // increment the offset
-        $args['offset'] = $offset; // set the new offset in the query
-
-        $posts = get_posts( $args ); // fetch the next batch of posts
+    // Loop through each post and delete it and its associated meta data and images
+    foreach ( $posts as $post ) {
+        wp_delete_post( $post->ID, true );
     }
 }
